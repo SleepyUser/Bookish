16039523cc700f27c228ea73c9a9d368ec0bd2d5
Tom Hanmer <Tom.Hanmer@softwire.com>
2022-07-12 15:44:21 +0100

    Allowed users to enter their own books

--- Bookish/Controllers/CatalogController.cs
+++ Bookish/Controllers/CatalogController.cs
@@ -13,9 +13,34 @@ public class CatalogController : Controller
     }
     
     // GET
-    public IActionResult BookEntry()
+    public IActionResult BookEntry(BookInputModel model)
     {
+        string bookName = model.BookName;
+        string isbn = model.ISBN;
+        string publisher = model.Publisher;
+        DateTime datePublished = model.DatePublished;
+        string[] authorNames = model.Author.Split(" ");
         using (var context = new LibraryContext())
+        {
+            var author = new Author()
+            {
+                AuthorSurname = authorNames[1],
+                AuthorForename = authorNames[0],
+            };
+            context.Authors.Add(author);
+            context.SaveChanges();
+            var book = new Book()
+            {
+                ISBN = isbn,
+                Title = bookName,
+                Publisher = publisher,
+                AuthorID = author.AuthorID,
+                DatePublished = datePublished
+            };
+            context.Books.Add(book);
+            context.SaveChanges();
+        }
+        /*using (var context = new LibraryContext())
         {
             var author = new Author()
             {
@@ -37,7 +62,7 @@ public class CatalogController : Controller
             
             context.Books.Add(book);
             context.SaveChanges();
-        }
+        }*/
         return View();
     }
     
---
+++ Bookish/Models/BookInputModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Bookish.Models;
+
+public class BookInputModel
+{
+    public string BookName { get; set; }
+    public string ISBN { get; set; }
+    public string Publisher { get; set; }
+    public DateTime DatePublished { get; set; }
+    public string Author { get; set; }
+}
