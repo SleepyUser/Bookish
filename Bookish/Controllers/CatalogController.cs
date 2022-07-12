2cc28a226eb95e67e2c92420e2d57ac663d607bd
Tom Hanmer <Tom.Hanmer@softwire.com>
2022-07-12 16:50:30 +0100

    made all user inputs so they can't enter nonsense for a new book

--- Bookish/Controllers/CatalogController.cs
+++ Bookish/Controllers/CatalogController.cs
@@ -20,7 +20,11 @@ public class CatalogController : Controller
         string isbn = model.ISBN;
         string publisher = model.Publisher;
         DateTime datePublished = model.DatePublished;
-        string[] authorNames = model.Author.Split(" ");
+        string[] authorNames = model.Author.Split(" ", 2);
+        if (authorNames.Length == 1)
+        {
+            authorNames = new string[] { authorNames[0], "" };
+        }
         using (var context = new LibraryContext())
         {
             var author = new Author()
@@ -41,29 +45,6 @@ public class CatalogController : Controller
             context.Books.Add(book);
             context.SaveChanges();
         }
-        /*using (var context = new LibraryContext())
-        {
-            var author = new Author()
-            {
-                AuthorSurname = "Sinha",
-                AuthorForename = "Sanjib",
-            };
-            
-            context.Authors.Add(author);
-            context.SaveChanges();
-            
-            var book = new Book()
-            {
-                ISBN = "9781720025191",
-                Title = "How to Build A PHP 7 Framework: With an Introduction to Composer, Interface, Trait, Horizontal Reuse of code, PDO, and MVC Pattern",
-                Publisher = "independent",
-                AuthorID = author.AuthorID,
-                DatePublished = new DateTime(2018,09,01)
-            };
-            
-            context.Books.Add(book);
-            context.SaveChanges();
-        }*/
         return View();
     }
     
@@ -74,20 +55,6 @@ public class CatalogController : Controller
         {
             bvm.CatalogEntries = context.Books.Include(b => b.Author)
                 .Include(b => b.CopyList).ToList();
-            /*var query = (from b in context.Books
-                join a in context.Authors on b.AuthorID equals a.AuthorID
-                select (
-                    //b.Title
-                    a.AuthorSurname
-                    //Forename = a.AuthorForename,
-                    //b.Publisher,
-                    //b.DatePublished,
-                    //b.ISBN
-                    //b.CopyList.Count
-                ));
-            //bvm.RawCatalogEntries = query;
-            var list = query.ToList();*/
-            
         }
         return View(bvm);
     }
