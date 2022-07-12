ee4a1cfb4e09a2e953cf97f6f2a745eaaf6a0f82
Tom Hanmer <Tom.Hanmer@softwire.com>
2022-07-12 11:13:36 +0100

    Created our database

---
+++ Bookish/Migrations/20220712100836_CreateLibraryDB.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bookish.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateLibraryDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Authors",
+                columns: table => new
+                {
+                    AuthorID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AuthorSurname = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AuthorForename = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AuthorID1 = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Authors", x => x.AuthorID);
+                    table.ForeignKey(
+                        name: "FK_Authors_Authors_AuthorID1",
+                        column: x => x.AuthorID1,
+                        principalTable: "Authors",
+                        principalColumn: "AuthorID");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Borrowers",
+                columns: table => new
+                {
+                    BorrowerID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Forename = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Surname = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BorrowerID1 = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Borrowers", x => x.BorrowerID);
+                    table.ForeignKey(
+                        name: "FK_Borrowers_Borrowers_BorrowerID1",
+                        column: x => x.BorrowerID1,
+                        principalTable: "Borrowers",
+                        principalColumn: "BorrowerID");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    BookID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ISBN = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AuthorID = table.Column<int>(type: "int", nullable: false),
+                    Publisher = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DatePublished = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.BookID);
+                    table.ForeignKey(
+                        name: "FK_Books_Authors_AuthorID",
+                        column: x => x.AuthorID,
+                        principalTable: "Authors",
+                        principalColumn: "AuthorID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Copies",
+                columns: table => new
+                {
+                    CopyID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BookID = table.Column<int>(type: "int", nullable: false),
+                    BorrowerID = table.Column<int>(type: "int", nullable: false),
+                    Comments = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Copies", x => x.CopyID);
+                    table.ForeignKey(
+                        name: "FK_Copies_Books_BookID",
+                        column: x => x.BookID,
+                        principalTable: "Books",
+                        principalColumn: "BookID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Copies_Borrowers_BorrowerID",
+                        column: x => x.BorrowerID,
+                        principalTable: "Borrowers",
+                        principalColumn: "BorrowerID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BorrowInstances",
+                columns: table => new
+                {
+                    BorrowInstanceID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BorrowerID = table.Column<int>(type: "int", nullable: false),
+                    CopyID = table.Column<int>(type: "int", nullable: false),
+                    BorrowDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ReturnDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsOverdue = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BorrowInstances", x => x.BorrowInstanceID);
+                    table.ForeignKey(
+                        name: "FK_BorrowInstances_Copies_CopyID",
+                        column: x => x.CopyID,
+                        principalTable: "Copies",
+                        principalColumn: "CopyID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Authors_AuthorID1",
+                table: "Authors",
+                column: "AuthorID1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_AuthorID",
+                table: "Books",
+                column: "AuthorID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Borrowers_BorrowerID1",
+                table: "Borrowers",
+                column: "BorrowerID1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BorrowInstances_CopyID",
+                table: "BorrowInstances",
+                column: "CopyID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Copies_BookID",
+                table: "Copies",
+                column: "BookID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Copies_BorrowerID",
+                table: "Copies",
+                column: "BorrowerID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BorrowInstances");
+
+            migrationBuilder.DropTable(
+                name: "Copies");
+
+            migrationBuilder.DropTable(
+                name: "Books");
+
+            migrationBuilder.DropTable(
+                name: "Borrowers");
+
+            migrationBuilder.DropTable(
+                name: "Authors");
+        }
+    }
+}
