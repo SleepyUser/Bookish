2415679812af5f6b894b571b93abc73b00046516
Tom Hanmer <Tom.Hanmer@softwire.com>
2022-07-13 09:28:12 +0100

    Took glen's table formatting, allowed users to enter the number of copies of a book they want to enter

--- Bookish/Controllers/CatalogController.cs
+++ Bookish/Controllers/CatalogController.cs
@@ -16,54 +16,60 @@ public class CatalogController : Controller
     // GET
     public IActionResult BookEntry(BookInputModel model)
     {
+        Book foundBook = new Book();
         string bookName = model.BookName;
         string isbn = model.ISBN;
         string publisher = model.Publisher;
         DateTime datePublished = model.DatePublished;
         string[] authorNames = model.Author.Split(" ");
+        int newCopies = model.NewCopies;
         using (var context = new LibraryContext())
         {
-            var author = new Author()
+            foundBook = context.Books.SingleOrDefault(a => a.ISBN == isbn);
+            if (foundBook != null)
             {
-                AuthorSurname = authorNames[1],
-                AuthorForename = authorNames[0],
-            };
-            context.Authors.Add(author);
-            context.SaveChanges();
-            var book = new Book()
+                for (int i = 0; i < newCopies; i++)
+                {
+                    var copy = new Copy()
+                    {
+                        BookID = foundBook.BookID,
+                        Comments = "This is a comment",
+                    };
+                    context.Copies.Add(copy);
+                    context.SaveChanges();
+                }
+            }
+            else
             {
-                ISBN = isbn,
-                Title = bookName,
-                Publisher = publisher,
-                AuthorID = author.AuthorID,
-                DatePublished = datePublished
-            };
-            context.Books.Add(book);
-            context.SaveChanges();
+                var author = new Author()
+                {
+                    AuthorSurname = authorNames[1],
+                    AuthorForename = authorNames[0],
+                };
+                context.Authors.Add(author);
+                context.SaveChanges();
+                var book = new Book()
+                {
+                    ISBN = isbn,
+                    Title = bookName,
+                    Publisher = publisher,
+                    AuthorID = author.AuthorID,
+                    DatePublished = datePublished
+                };
+                context.Books.Add(book);
+                context.SaveChanges();
+                for (int i = 0; i < newCopies; i++)
+                {
+                    var copy = new Copy()
+                    {
+                        BookID = book.BookID,
+                        Comments = "This is a comment"
+                    };
+                    context.Copies.Add(copy);
+                    context.SaveChanges();
+                }
+            }
         }
-        /*using (var context = new LibraryContext())
-        {
-            var author = new Author()
-            {
-                AuthorSurname = "Sinha",
-                AuthorForename = "Sanjib",
-            };
-            
-            context.Authors.Add(author);
-            context.SaveChanges();
-            
-            var book = new Book()
-            {
-                ISBN = "9781720025191",
-                Title = "How to Build A PHP 7 Framework: With an Introduction to Composer, Interface, Trait, Horizontal Reuse of code, PDO, and MVC Pattern",
-                Publisher = "independent",
-                AuthorID = author.AuthorID,
-                DatePublished = new DateTime(2018,09,01)
-            };
-            
-            context.Books.Add(book);
-            context.SaveChanges();
-        }*/
         return View();
     }
     
